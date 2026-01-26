37ec7988331cafac6842b3af8cd67d9dec98b827
Zhiqiang ZHOU <im@strrl.dev>
2026-01-25 21:15:58 -0800

    fix: change dropdown menu to open below instead of right

--- apps/s3-studio/src/components/profiles/profile-selector.tsx
+++ apps/s3-studio/src/components/profiles/profile-selector.tsx
@@ -81,7 +81,7 @@ export function ProfileSelector({ onAddNew, onEdit, onDisconnect }: ProfileSelec
                         <MoreVertical className="size-3.5" />
                       </Button>
                     </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end" side="right">
+                    <DropdownMenuContent align="end" side="bottom">
                       <DropdownMenuItem onClick={() => onEdit(profileId)}>
                         <Settings className="size-4" />
                         Settings
