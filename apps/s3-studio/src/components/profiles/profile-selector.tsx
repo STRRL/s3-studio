18d09eb1d26d59fd6a7c594bf78d3a932b4a4280
Zhiqiang ZHOU <im@strrl.dev>
2026-01-25 21:36:52 -0800

    fix: center-align dropdown menu with button

--- apps/s3-studio/src/components/profiles/profile-selector.tsx
+++ apps/s3-studio/src/components/profiles/profile-selector.tsx
@@ -81,7 +81,7 @@ export function ProfileSelector({ onAddNew, onEdit, onDisconnect }: ProfileSelec
                         <MoreVertical className="size-3.5" />
                       </Button>
                     </DropdownMenuTrigger>
-                    <DropdownMenuContent align="start" side="right">
+                    <DropdownMenuContent align="center" side="right">
                       <DropdownMenuItem onClick={() => onEdit(profileId)}>
                         <Settings className="size-4" />
                         Settings
